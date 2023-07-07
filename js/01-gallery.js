eb86322ec31a4601423465b49f2b8015061c4286
Chapelk1 <klepach154789@gmail.com>
2023-07-07 03:23:54 +0300

    addJsCode

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,76 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const refs = {
+    listEl: document.querySelector(".gallery"),   
+} 
+refs.listEl.insertAdjacentHTML("beforeend", createAllImgItemList(galleryItems));
+refs.listEl.addEventListener("click", createModalImg);
+
+function createModalImg(event) {
+  event.preventDefault();
+
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+
+  const originalSizeImg = event.target.dataset.source;
+  const modalImg = basicLightbox.create(
+    `
+        <img width="1400" height="900" src="${originalSizeImg}">
+        `,
+    { onShow: createTargetModalClick, onClose: removeTargetModalClick }
+  );
+
+  modalImg.show();
+
+  function createTargetModalClick() {
+    window.addEventListener("keyup", onTargetModalClick);
+  }
+  function removeTargetModalClick() {
+    window.removeEventListener("keyup", onTargetModalClick);
+  }
+  function onTargetModalClick(e) {
+    console.log(e.code);
+    if (e.code === "Escape") {
+        modalImg.close();
+        
+    }
+  }
+}
+
+function createAllImgItemList(gallery) {
+return gallery
+  .map(({ preview, original, description }) => {
+    return `
+        <li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+                />
+            </a>
+        </li>
+        `;
+  })
+  .join("");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,30 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const refs = {
+  listEl: document.querySelector(".gallery"),
+}; 
+
+refs.listEl.insertAdjacentHTML("beforeend", createAllImgItemList(galleryItems));
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionDelay: 250,
+  captionsData: `alt`,
+  
+});
+
+
+function createAllImgItemList(gallery) {
+  return gallery
+    .map(({ preview, original, description }) => {
+      return `
+        <li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>
+        </li>
+        `;
+    })
+    .join("");
+}
+
